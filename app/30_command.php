e703a3bb155732d3eab6dc0c248fe134c9522f30
Matthes <matthes@leuffen.de>
2022-02-15 22:38:38 +0100

    update

--- app/30_command.php
+++ app/30_command.php
@@ -10,11 +10,11 @@ AppLoader::extend(function (BraceApp $app) {
 
 
     $app->command->addCommand("send", function(array $argv) {
-        $runner = new FileStatsRunner();
-        $runner->runAll($argv[0] ?? 0);
+        //$runner = new FileStatsRunner();
+        //$runner->runAll($argv[0] ?? 0);
     });
 
     // Send yesterdays report (specified by 1)
-    $app->command->addInterval(86400, "send", [1], true);
+    $app->command->addInterval("5 0 * * *", "send", [1], true);
 
 });
