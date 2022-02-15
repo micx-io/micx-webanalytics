a99f423d4f883f28791933d7629e53a6b70bdd27
Matthes <matthes@leuffen.de>
2022-02-15 21:24:14 +0100

    update

--- app/30_command.php
+++ app/30_command.php
@@ -9,12 +9,12 @@ use Micx\FormMailer\Stats\FileStatsRunner;
 AppLoader::extend(function (BraceApp $app) {
 
 
-    $app->command->addCommand("run", function(array $argv) {
+    $app->command->addCommand("send", function(array $argv) {
         $runner = new FileStatsRunner();
         $runner->runAll($argv[0] ?? 0);
     });
 
     // Send yesterdays report (specified by 1)
-    $app->command->addInterval(5, "run", [1], true);
+    $app->command->addInterval(86400, "send", [1], true);
 
 });
