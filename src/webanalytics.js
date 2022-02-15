fe3d78feba562ce491997f0207337029d5b85d86
Matthes <matthes@leuffen.de>
2022-02-15 02:35:39 +0100

    update

--- src/webanalytics.js
+++ src/webanalytics.js
@@ -13,7 +13,7 @@
   let endpoint_url="%%ENDPOINT_URL%%";
   let session_id="%%SESSION_ID%%";
   let startTime = +new Date();
-
+  let wakeups = 0;
 
   document.addEventListener("visibilitychange", () => {
     if (document.visibilityState !== 'hidden')
@@ -26,7 +26,8 @@
       user_agent: window.navigator.userAgent,
       language: window.navigator.language,
       screen: screen.height + "x" + screen.width,
-      duration: (+new Date() - startTime) / 1000
+      duration: (+new Date() - startTime) / 1000,
+      wakeups: wakeups++
     }
 
     navigator.sendBeacon(endpoint_url, JSON.stringify(data));
